5ba869e6041b901f84f0c204dab4594064162c61
Makhmutov Vladimir <mahmutov.vladimir@urbantelematics.com>
2024-09-09 17:19:01 +0700

    add audio MOCK data volume control styling

--- src/CONSTANTS/weather-mock.js
+++ src/CONSTANTS/weather-mock.js
@@ -4,21 +4,27 @@ import rainyImg from "../assets/rainy-bg.jpg";
 import summerIco from "../assets/icons/sun.svg";
 import rainyIco from "../assets/icons/cloud-rain.svg";
 import winterIco from "../assets/icons/cloud-snow.svg";
+import summerMp3 from "../assets/sounds/summer.mp3";
+import rainMp3 from "../assets/sounds/rain.mp3";
+import winterMp3 from "../assets/sounds/summer.mp3";
 
 export const weather = {
     summer: {
         bg: summerImg,
         icon: summerIco,
+        audio: new Audio(summerMp3),
         id: 1
     },
     rain: {
         bg: rainyImg,
         icon: rainyIco,
+        audio: new Audio(rainMp3),
         id: 2
     },
     winter: {
         bg: winterImg,
         icon: winterIco,
+        audio: new Audio(winterMp3),
         id: 3
     }
 }
